adac06660b872919e1983d6db2a154c343423a39
Mark <me@jsman.ru>
2013-10-08 09:13:09 +0300

    normal way to write package.json

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Basic template description.
-exports.description = 'Create Browserify2 project.';
+exports.description = 'Create Browserify project';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '';
@@ -12,18 +12,27 @@ exports.warnOn = 'Gruntfile.js';
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
-  init.process({
+	init.process({
 
-  }, [], function(err, props) {
+	}, [
+		init.prompt('name'),
+		init.prompt('version')
+	], function(err, props) {
 
-    // Files to copy (and process).
-    var files = init.filesToCopy(props);
+		props.devDependencies = {
+			"grunt-contrib-watch": "~0.5.3",
+			"grunt": "~0.4.1",
+			"grunt-browserify": "~1.2.8"
+		};
 
-    // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
 
-    // All done!
-    done();
-  });
+		var files = init.filesToCopy(props);
+
+		init.copyAndProcess(files, props);
+
+		init.writePackageJSON('package.json', props);
+
+		done();
+	});
 
 };
